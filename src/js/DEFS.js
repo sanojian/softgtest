d420e39ca27b842299530b7a508ffbadbd83a356
Jonas Olmstead <>
2023-11-05 10:18:39 +0100

    draw a card

--- src/js/DEFS.js
+++ src/js/DEFS.js
@@ -1,3 +1,4 @@
 const DEFS = {
 
+	VERSION: '0.0.1'
 };
--- src/js/scenes/PlayScene.js
+++ src/js/scenes/PlayScene.js
@@ -19,7 +19,41 @@ class PlayScene extends Phaser.Scene {
 
 		console.log('Game started');
 
+		const gfx = this.make.graphics({x: 0, y: 0, add: false});
 
+		gfx.lineStyle(6, 0xffffff, 1.0);
+		gfx.strokeRect(0, 0, 140, 200);
+
+		//gfx.generateTexture('cardGraphic', 100, 200);
+		let style = {
+			font: "24px DINCondensed-Bold",
+			align: "center",
+			fill: "#fff"
+		};
+		const cardText1 = this.add.text(10, 10, '9\n♥️', style)
+			.setOrigin(0);
+		const cardText2 = this.add.text(130, 190, '9\n♥️', style)
+			.setOrigin(1)
+			.setFlipX(true)
+			.setFlipY(true);
+		style.font = "32px DINCondensed-Bold";
+		const cardText3 = this.add.text(70, 100, '♥️', style)
+			.setOrigin(0.5);
+		let renderTexture = this.add.renderTexture(0, 0, 140, 200);
+		
+		renderTexture.draw(gfx);
+		renderTexture.draw(cardText1);
+		renderTexture.draw(cardText2);
+		renderTexture.draw(cardText3);
+		renderTexture.saveTexture('cardGraphic');
+
+		gfx.destroy();
+		cardText1.destroy();
+		cardText2.destroy();
+		cardText3.destroy();
+		renderTexture.destroy();
+
+		const image = this.add.image(200, 200, 'cardGraphic');
 	}
 	
 }
